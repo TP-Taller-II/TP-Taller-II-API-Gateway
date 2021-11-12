c9fc77eef5ad7be0259c08402c3e38c8816d02da
Rodrigo Souto <rnsoutob@gmail.com>
2021-11-12 01:01:28 -0300

    feat(api): all calls to courses

--- api_gateway/namespaces/course/namespace.py
+++ api_gateway/namespaces/course/namespace.py
@@ -10,7 +10,7 @@ from api_gateway.helpers.logger import logger
 ns = Namespace("Course", description="Courses operations")
 
 
-def call_courses():
+def call_courses(payload):
     logger.info('Courses Call')
 
     if 'Authorization' not in request.headers:
@@ -19,7 +19,7 @@ def call_courses():
     token = request.headers['Authorization']
     path = request.path.split('/api')[1]
     method = request.method.lower()
-    res_body, res_status_code = course_client.call(method, path, token, None)
+    res_body, res_status_code = course_client.call(method, path, token, payload)
     return res_body, res_status_code
 
 
@@ -29,22 +29,22 @@ class Course2Resource(Resource):
     @ns.doc('get_call_courses')
     def get(self, p1, p2):
         """Get Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('post_call_courses')
     def post(self, p1, p2):
         """Post Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('patch_call_courses')
     def patch(self, p1, p2):
         """Patch Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('delete_call_courses')
     def delete(self, p1, p2):
         """Delete Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
 
 @ns.route('/<string:p1>/<string:p2>/<string:p3>')
@@ -53,22 +53,22 @@ class Course3Resource(Resource):
     @ns.doc('get_call_courses')
     def get(self, p1, p2, p3):
         """Get Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('post_call_courses')
     def post(self, p1, p2, p3):
         """Post Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('patch_call_courses')
     def patch(self, p1, p2, p3):
         """Patch Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('delete_call_courses')
     def delete(self, p1, p2, p3):
         """Delete Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
 
 @ns.route('/<string:p1>/<string:p2>/<string:p3>/<string:p4>')
@@ -77,22 +77,22 @@ class Course4Resource(Resource):
     @ns.doc('get_call_courses')
     def get(self, p1, p2, p3, p4):
         """Get Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('post_call_courses')
     def post(self, p1, p2, p3, p4):
         """Post Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('patch_call_courses')
     def patch(self, p1, p2, p3, p4):
         """Patch Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('delete_call_courses')
     def delete(self, p1, p2, p3, p4):
         """Delete Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
 
 @ns.route('/<string:p1>/<string:p2>/<string:p3>/<string:p4>/<string:p5>')
@@ -101,22 +101,22 @@ class Course5Resource(Resource):
     @ns.doc('get_call_courses')
     def get(self, p1, p2, p3, p4, p5):
         """Get Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('post_call_courses')
     def post(self, p1, p2, p3, p4, p5):
         """Post Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('patch_call_courses')
     def patch(self, p1, p2, p3, p4, p5):
         """Patch Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('delete_call_courses')
     def delete(self, p1, p2, p3, p4, p5):
         """Delete Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
 
 @ns.route('/<string:p1>/<string:p2>/<string:p3>/<string:p4>/<string:p5>/<string:p6>')
@@ -125,19 +125,71 @@ class Course6Resource(Resource):
     @ns.doc('get_call_courses')
     def get(self, p1, p2, p3, p4, p5, p6):
         """Get Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('post_call_courses')
     def post(self, p1, p2, p3, p4, p5, p6):
         """Post Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('patch_call_courses')
     def patch(self, p1, p2, p3, p4, p5, p6):
         """Patch Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
 
     @ns.doc('delete_call_courses')
     def delete(self, p1, p2, p3, p4, p5, p6):
         """Delete Courses Call"""
-        return call_courses()
+        return call_courses(ns.payload)
+
+
+@ns.route(
+    '/<string:p1>/<string:p2>/<string:p3>/<string:p4>/<string:p5>/<string:p6>/<string:p7>'
+)
+@ns.header('Authorization', 'Authorization Token')
+class Course7Resource(Resource):
+    @ns.doc('get_call_courses')
+    def get(self, p1, p2, p3, p4, p5, p6, p7):
+        """Get Courses Call"""
+        return call_courses(ns.payload)
+
+    @ns.doc('post_call_courses')
+    def post(self, p1, p2, p3, p4, p5, p6, p7):
+        """Post Courses Call"""
+        return call_courses(ns.payload)
+
+    @ns.doc('patch_call_courses')
+    def patch(self, p1, p2, p3, p4, p5, p6, p7):
+        """Patch Courses Call"""
+        return call_courses(ns.payload)
+
+    @ns.doc('delete_call_courses')
+    def delete(self, p1, p2, p3, p4, p5, p6, p7):
+        """Delete Courses Call"""
+        return call_courses(ns.payload)
+
+
+@ns.route(
+    '/<string:p1>/<string:p2>/<string:p3>/<string:p4>/<string:p5>/<string:p6>/<string:p7>/<string:p8>'
+)
+@ns.header('Authorization', 'Authorization Token')
+class Course8Resource(Resource):
+    @ns.doc('get_call_courses')
+    def get(self, p1, p2, p3, p4, p5, p6, p7, p8):
+        """Get Courses Call"""
+        return call_courses(ns.payload)
+
+    @ns.doc('post_call_courses')
+    def post(self, p1, p2, p3, p4, p5, p6, p7, p8):
+        """Post Courses Call"""
+        return call_courses(ns.payload)
+
+    @ns.doc('patch_call_courses')
+    def patch(self, p1, p2, p3, p4, p5, p6, p7, p8):
+        """Patch Courses Call"""
+        return call_courses(ns.payload)
+
+    @ns.doc('delete_call_courses')
+    def delete(self, p1, p2, p3, p4, p5, p6, p7, p8):
+        """Delete Courses Call"""
+        return call_courses(ns.payload)
--- tests/test_api.py
+++ tests/test_api.py
@@ -8,9 +8,6 @@ import pytest
 
 from api_gateway.app import create_app
 
-# import requests
-# from pytest_mock import mocker
-
 logger = logging.getLogger(__name__)
 
 
@@ -42,8 +39,255 @@ def test_root(client):
 
 
 def test_get_courses(client, mocker):
-    mocker.patch('requests.get', return_value=ResponseMock(201, []))
+    mock_call = mocker.patch(
+        'requests.get', return_value=ResponseMock(200, ['course1', 'course2'])
+    )
+
     response = client.get("/api/courses/v1/courses")
 
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == ['course1', 'course2']
+
+
+def test_post_courses(client, mocker):
+    mock_call = mocker.patch(
+        'requests.post', return_value=ResponseMock(201, {'resource': {'id': '1'}})
+    )
+
+    response = client.post("/api/courses/v1/courses", json={'name': 'Fiesta'})
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
     assert response._status_code == 201
-    assert json.loads(response.data) == []
+    assert json.loads(response.data) == {'resource': {'id': '1'}}
+
+
+def test_get_course(client, mocker):
+    mock_call = mocker.patch(
+        'requests.get', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.get("/api/courses/v1/courses/1")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_patch_course(client, mocker):
+    mock_call = mocker.patch(
+        'requests.patch', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.patch("/api/courses/v1/courses/1", json={'name': 'Fiesta'})
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_delete_course(client, mocker):
+    mock_call = mocker.patch(
+        'requests.delete', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.delete("/api/courses/v1/courses/1")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_get_exams(client, mocker):
+    mock_call = mocker.patch(
+        'requests.get', return_value=ResponseMock(200, [{'id': '1'}])
+    )
+
+    response = client.get("/api/courses/v1/courses/1/exams")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == [{'id': '1'}]
+
+
+def test_post_exams(client, mocker):
+    mock_call = mocker.patch(
+        'requests.post', return_value=ResponseMock(201, {'resource': {'id': '1'}})
+    )
+
+    response = client.post("/api/courses/v1/courses/1/exams", json={'name': 'Fiesta'})
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
+    assert response._status_code == 201
+    assert json.loads(response.data) == {'resource': {'id': '1'}}
+
+
+def test_get_exam(client, mocker):
+    mock_call = mocker.patch(
+        'requests.get', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.get("/api/courses/v1/courses/1/exams/1")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_patch_exam(client, mocker):
+    mock_call = mocker.patch(
+        'requests.patch', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.patch(
+        "/api/courses/v1/courses/1/exams/1", json={'name': 'Fiesta'}
+    )
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_delete_exam(client, mocker):
+    mock_call = mocker.patch(
+        'requests.delete', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.delete("/api/courses/v1/courses/1/exams/1")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_get_students(client, mocker):
+    mock_call = mocker.patch(
+        'requests.get', return_value=ResponseMock(200, [{'id': '1'}])
+    )
+
+    response = client.get("/api/courses/v1/courses/1/students")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == [{'id': '1'}]
+
+
+def test_post_students(client, mocker):
+    mock_call = mocker.patch(
+        'requests.post', return_value=ResponseMock(201, {'resource': {'id': '1'}})
+    )
+
+    response = client.post(
+        "/api/courses/v1/courses/1/students", json={'name': 'Fiesta'}
+    )
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
+    assert response._status_code == 201
+    assert json.loads(response.data) == {'resource': {'id': '1'}}
+
+
+def test_delete_student(client, mocker):
+    mock_call = mocker.patch(
+        'requests.delete', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.delete("/api/courses/v1/courses/1/students/1")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_get_professors(client, mocker):
+    mock_call = mocker.patch(
+        'requests.get', return_value=ResponseMock(200, [{'id': '1'}])
+    )
+
+    response = client.get("/api/courses/v1/courses/1/professors")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == [{'id': '1'}]
+
+
+def test_post_professors(client, mocker):
+    mock_call = mocker.patch(
+        'requests.post', return_value=ResponseMock(201, {'resource': {'id': '1'}})
+    )
+
+    response = client.post(
+        "/api/courses/v1/courses/1/professors", json={'name': 'Fiesta'}
+    )
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
+    assert response._status_code == 201
+    assert json.loads(response.data) == {'resource': {'id': '1'}}
+
+
+def test_delete_professor(client, mocker):
+    mock_call = mocker.patch(
+        'requests.delete', return_value=ResponseMock(200, {'id': '1'})
+    )
+
+    response = client.delete("/api/courses/v1/courses/1/professors/1")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == {'id': '1'}
+
+
+def test_get_exam_resolutions(client, mocker):
+    mock_call = mocker.patch(
+        'requests.get', return_value=ResponseMock(200, [{'id': '1'}])
+    )
+
+    response = client.get("/api/courses/v1/courses/1/exams/1/resolutions")
+
+    mock_call.assert_called_once_with(mocker.ANY, json={}, headers=mocker.ANY)
+    assert response._status_code == 200
+    assert json.loads(response.data) == [{'id': '1'}]
+
+
+def test_post_exam_resolutions(client, mocker):
+    mock_call = mocker.patch(
+        'requests.post', return_value=ResponseMock(201, {'resource': {'id': '1'}})
+    )
+
+    response = client.post(
+        "/api/courses/v1/courses/1/exams/1/resolutions", json={'name': 'Fiesta'}
+    )
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
+    assert response._status_code == 201
+    assert json.loads(response.data) == {'resource': {'id': '1'}}
+
+
+def test_post_exam_resolutions_evaluate(client, mocker):
+    mock_call = mocker.patch(
+        'requests.post', return_value=ResponseMock(201, {'resource': {'id': '1'}})
+    )
+
+    response = client.post(
+        "/api/courses/v1/courses/1/exams/1/resolutions/1/evaluate",
+        json={'name': 'Fiesta'},
+    )
+
+    mock_call.assert_called_once_with(
+        mocker.ANY, json={'name': 'Fiesta'}, headers=mocker.ANY
+    )
+    assert response._status_code == 201
+    assert json.loads(response.data) == {'resource': {'id': '1'}}
