6e2203d37744bcf5d434660bce1525b8a4299c46
Rodrigo Souto <rnsoutob@gmail.com>
2021-12-27 02:30:12 -0300

    chore(datadog): installing everything

--- heroku/heroku-entrypoint.sh
+++ heroku/heroku-entrypoint.sh
@@ -3,4 +3,4 @@
 datadog-agent run > /dev/null &
 /opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
 /opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
-poetry run gunicorn -w 4 --log-level=debug --bind 0.0.0.0:$PORT "api_gateway.app:create_app()"
+ddtrace-run poetry run gunicorn -w 4 --log-level=debug --bind 0.0.0.0:$PORT "api_gateway.app:create_app()"
